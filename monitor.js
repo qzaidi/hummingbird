1221c75e06232bb0e3b4d9a6f88cbf5b27566406
Qasim <qasim@zaidi.me>
2011-07-16 11:15:56 +0530

    basic auth

--- monitor.js
+++ monitor.js
@@ -1,15 +1,35 @@
 var sys = require('sys'),
     express = require('express'),
-    weekly = require('weekly');
+    weekly = require('weekly'),
+    users = require('users');
 
 var app = express.createServer();
 
 app.configure(function(){
   app.set('root', __dirname);
   app.set('db', db);
+  app.set('view options', { layout: false });
+  app.set('view engine', 'ejs');
+  app.set('views', __dirname + '/views');
   app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({  secret: '1a2b3c4d5e6f',
+			     store: require('connect').session.MemoryStore({ reapInterval: 60000 * 10 })
+			  }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public', { maxAge: 86400 }));
+});
+
+function needsAuth(req,res,next) {
+  if (req.session.user) {
+    next();
+  } else {
+    res.redirect('/login?redir=' + req.url);
+  }
+}
+
+app.get('/', needsAuth, function(req,res) {
+  res.render('index', { config: config });
 });
 
 app.get('/json/:id', function(req,res) {
@@ -22,15 +42,24 @@ app.get('/json/:id', function(req,res) {
   });
 });
 
+app.get('/cookie/', function(req,res) {
+  res.render('cookie',{ cookie: req.cookie });
+});
+
 app.post('/login', function(req,res) {
-    if(req.body.password == 'password') {
-      res.cookie('not_secret', req.body.password);
-      sys.log("Auth succeeded for " + req.body.username);
-      res.redirect('/');
-    } else {
+  users.authenticate(req.body.username,req.body.password, function(user) {
+    if (user) { 
+       req.session.user = user;
+       res.redirect(req.body.redir || '/');
+    } else { 
       sys.log("Auth failed for " + req.body.username);
-      res.redirect('/login');
+      res.render('login');
     }
   });
+});
+
+app.get('/login',function(req,res) {
+  res.render('login');
+});
 
 app.listen(config.dashboard_port);
